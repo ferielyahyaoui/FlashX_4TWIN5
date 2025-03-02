e82cececc30399aaf7322234cbe02c88eeb1cac6
elaasboui <100567449+elaasboui@users.noreply.github.com>
2025-03-02 21:27:58 +0100

    Destinations

---
+++ react-tailwind/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/*"],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3498db",
+        secondary: "#f1c40f",
+        dark: "#2c3e50",
+        light: "#ecf0f1",
+        },
+
+      },
+  },
+  plugins: [],
+}
+
