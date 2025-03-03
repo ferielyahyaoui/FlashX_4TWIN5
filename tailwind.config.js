bed1324b6dbc4df4758fde1e0f04fd408f5a4a56
ahmed benhmida <benhmida.ahmed1@esprit.tn>
2025-03-03 03:01:20 +0100

    last

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: ["./public/*","./src/*"],
   theme: {
+    screens: {
+      md: { max: "1050px" }, // Medium screens
+      sm: { max: "550px" },   // Small screens
+    },
     extend: {
       colors:{
-        primary: "#00A0B1",
-        secondary:"#00CBE0",
+        primary: "#00a0b1",
+        secondary:"#00a0b1",
         tertiary:"#68d5e0",
       },
       fontFamily: {
