482b4a8a0d5385b8ffe122730aab5a115afa5f65
oudayoueslati <147772540+oudayoueslati@users.noreply.github.com>
2025-03-03 02:30:20 +0100

    mise_a_jour

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class', 
+  theme: {
+    extend: {
+      colors: {
+        "custom-cyan": "#00a0b1", 
+        "dark-custom-cyan": "#007A87", 
+        "light-cyan": "#E0F7FA",
+        "dark-gray": "#333",
+        "medium-gray": "#666",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        rajdhani: ["Rajdhani", "sans-serif"],
+      },
+      fontSize: {
+        "custom-18": "18px",
+        "custom-24": "24px",
+        "custom-40": "40px",
+      },
+      lineHeight: {
+        "custom-24": "24px",
+      },
+      boxShadow: {
+        custom: "0px 16px 40px 0px rgba(112, 144, 176, 0.2)",
+      },
+    },
+  },
+  plugins: [],
+};
